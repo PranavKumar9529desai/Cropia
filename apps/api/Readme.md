31421b3d151eb5fdd6914791ba063a4ca4cb6151
Your Name <fullstackwebdeveloper123@gmail.com>
2025-12-12 19:47:07 +0530

    feat: some smaller changes in the Readme.md

--- apps/api/Readme.md
+++ apps/api/Readme.md
@@ -1,3 +1,4 @@
 ### OpenApi spec for BetterAuth
 
 http://localhost:4000/api/auth/reference
+- to view all the endpoint used by the betterauth.
--- apps/api/src/auth.ts
+++ apps/api/src/auth.ts
@@ -6,7 +6,7 @@ import { transporter } from "./utils/email";
 
 // Define sender once
 const SENDER_EMAIL = `"Cropia Team" <${process.env.GMAIL_USER}>`;
-const frontendUrl = process.env.FRONTEND_URL_FARMER_APP || "http://localhost:5000";
+const frontendUrl = process.env.FRONTEND_URL || "http://localhost:5000";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:4000",
