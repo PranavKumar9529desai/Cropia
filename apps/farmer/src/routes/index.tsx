ba7fea0e3fbe73d999a827e7a6a22afcc65b30d9
Your Name <fullstackwebdeveloper123@gmail.com>
2025-12-20 13:04:54 +0530

    feat : landing page redirected are added in staging.

--- apps/farmer/src/routes/index.tsx
+++ apps/farmer/src/routes/index.tsx
@@ -1,30 +1,37 @@
 import { authClient } from "@/lib/auth/auth-client";
 import { Button } from "@repo/ui/components/button";
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  beforeLoad: async () => {
-    const { data } = await authClient.getSession();
-    const isLoggedIn = data?.user ? true : false;
-    if (!isLoggedIn) {
-      redirect({ to: "/sign-in" });
-    }
-  },
   component: IndexComponent,
 });
 
 function IndexComponent() {
+  const session = authClient.useSession();
   return (
-    <>
-      <Button
-        className="max-w-xs w-full text-center"
-        title="signOut"
-        onClick={() => {
-          authClient.signOut();
-        }}
-      >
-        Sign Out
-      </Button>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      {session.data ? (
+        <>
+          <Link to="/dashboard">
+            <Button className="max-w-xs w-full text-center">
+              Go to Dashboard
+            </Button>
+          </Link>
+          <Button
+            className="max-w-xs w-full text-center"
+            title="signOut"
+            onClick={() => {
+              authClient.signOut();
+            }}
+          >
+            Sign Out
+          </Button>
+        </>
+      ) : (
+        <Link to="/sign-in">
+          <Button className="max-w-xs w-full text-center">Sign In</Button>
+        </Link>
+      )}
+    </div>
   );
 }
